6ad61f9afae3ea11ee44a35b4c27134dfe48bd42
MarcusElg <marcusaccounts@yahoo.se>
2024-07-27 20:14:26 +0200

    Register enchantment reimagined block

--- src/main/java/enchantingreimagined/EnchantingReimagined.java
+++ src/main/java/enchantingreimagined/EnchantingReimagined.java
@@ -2,6 +2,8 @@ package enchantingreimagined;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -12,6 +14,7 @@ import net.minecraft.text.Text;
 public class EnchantingReimagined implements ModInitializer {
 	public static TooltippedItem ENCHANTMENT_DUST;
 	public static TooltippedItem CURSER_SCRUBBER;
+	public static EnchantingWorkstationBlock ENCHANTING_WORKSTATION;
 
 	public static Identifier get_id(String id) {
 		return Identifier.of("enchanting_reimagined", id);
@@ -32,14 +35,21 @@ public class EnchantingReimagined implements ModInitializer {
 	}
 
 	private void registerBlocks() {
+		ENCHANTING_WORKSTATION = new EnchantingWorkstationBlock();
+		registerBlock(ENCHANTING_WORKSTATION, "enchanting_workstation");
+	}
 
+	private void registerBlock(Block block, String name) {
+		Registry.register(Registries.BLOCK, get_id(name), block);
+		Registry.register(Registries.ITEM, get_id(name), new BlockItem(block, new Item.Settings()));
 	}
 
 	private void registerItemGroup() {
 		Registry.register(Registries.ITEM_GROUP, get_id("enchanting_reimagined"), FabricItemGroup.builder()
-				.icon(() -> new ItemStack(ENCHANTMENT_DUST))
+				.icon(() -> new ItemStack(ENCHANTING_WORKSTATION))
 				.displayName(Text.translatable("itemGroup.enchating_reimagined.enchanting_reimagined"))
 				.entries((context, entries) -> {
+					entries.add(new ItemStack(ENCHANTING_WORKSTATION));
 					entries.add(new ItemStack(ENCHANTMENT_DUST));
 					entries.add(new ItemStack(CURSER_SCRUBBER));
 				})
---
+++ src/main/java/enchantingreimagined/EnchantingWorkstationBlock.java
@@ -0,0 +1,11 @@
+package enchantingreimagined;
+
+import net.minecraft.block.Block;
+
+public class EnchantingWorkstationBlock extends Block {
+
+    public EnchantingWorkstationBlock() {
+        super(Block.Settings.create());
+    }
+
+}
